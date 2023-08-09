8375da0891581b9aa25ef944e52df7486d02cb70
sunnycharkhwal <sunny.charkhwal@gmail.com>
2023-08-10 00:06:35 +0530

    ev charging page

--- contactForm/contact_form.php
+++ contactForm/contact_form.php
@@ -1,14 +1,14 @@
 <?php
 //checking if recived a post request
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  
+
     ob_start();
     require_once("email_template.php");
     $body = ob_get_contents();
-      print_r($body);
+    print_r($body);
     ob_end_clean();
     //die($body);
-    
+
     $from = 'info@adyanshsolutions.com';
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // $headers .= "From: " . $from . "\r\n";
     // $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
     //preparing email prameters
-    $to = "sunny.charkhwal@gmail.com";
+    $to = "adyansh.solutions@gmail.com";
     // $to2 = "hackinghunter1995@gmail.com";
     $subject = "New enquiry";
     //sending email to user
--- ev_chargers.php
+++ ev_chargers.php
@@ -1,4 +1,4 @@
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
 <!-- all page top header -->
 <div class="top_header_all_page mb-5">
     <div class="top_header_all_page_img" style="background-image: url('img/evt.jpeg');"></div>
@@ -14,114 +14,95 @@
     </div>
 </div>
 <!-- all page top header -->
-<section class="container pb-5">
-    <div class="row g-md-4 g-3">
-        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-            <div>
-                <div class="title_index_about title_index_about2">
-                    <h3>
-                        X0 Specifications
-                    </h3>
+<section class=" mb-5 container">
+    <div class="title_index_about title_index_about2">
+        <p><i class="fas fa-circle"></i> Demo title</p>
+        <h3>
+            demo title
+        </h3>
+    </div>
+    <div class="coltact_us_about index_about_text_div">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, quas illo
+            <br>
+            Lorem ipsum dolor sit amet.
+        </p>
+    </div>
+</section>
+<!-- list  -->
+<section class=" mb-5 container">
+    <div class="accordion ev_table_list" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button ev_list_btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    3.3 KW Portable Type 2 (Guns 1)
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <div>
+                        <table class="table table-striped table-hover text-center">
+                            <thead>
+                                <tr>
+                                    <th scope="col">title</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">title 3</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>demo</td>
+                                    <td>demo</td>
+                                    <td>demo</td>
+                                </tr>
+                                <tr>
+                                    <td>demo</td>
+                                    <td>demo</td>
+                                    <td>demo</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <img class="management_system_img" src="img/ec1.webp" alt="img">
             </div>
         </div>
-        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-            <div>
-                <div class="title_index_about title_index_about2">
-                    <h3>
-                        X0 Specifications
-                    </h3>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed ev_list_btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    7.4 KW Portable Type 2 (Guns 1)
+                </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <div>
+                        <table class="table table-striped table-hover text-center">
+                            <thead>
+                                <tr>
+                                    <th scope="col">title</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">title 3</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>demo</td>
+                                    <td>demo</td>
+                                    <td>demo</td>
+                                </tr>
+                                <tr>
+                                    <td>demo</td>
+                                    <td>demo</td>
+                                    <td>demo</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <img class="management_system_img" src="img/ec2.webp" alt="img">
-
             </div>
         </div>
     </div>
 </section>
-<!-- add section -->
-<section class="add_section mb-5">
-
-    <div class=" container">
-        <div class="title_index_about title_index_about2">
-            <p><i class="fas fa-circle "></i> POWER SWAP STATION </p>
-            <h3>
-                AC Electric Car Chargers
-            </h3>
-        </div>
-        <div class="row g-4">
-            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 add_section_col" data-aos="zoom-out"
-                data-aos-duration="1000">
-                <img src="img/s1.webp" alt="cr">
-            </div>
-            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 add_section_col" data-aos="zoom-out"
-                data-aos-duration="1000">
-                <img src="img/s2.webp" alt="cr">
-            </div>
-            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 add_section_col" data-aos="zoom-out"
-                data-aos-duration="1000">
-                <img src="img/s3.webp" alt="cr">
-            </div>
-        </div>
-    </div>
-</section>
-<!-- add section end -->
-<section class="container pb-5">
-    <div class="title_index_about title_index_about2">
-        <h3>
-            Our Plug And Play Charger Series
-        </h3>
-    </div>
-    <div class="row">
-        <div class="photo_list_div">
-            <img src="img/n3.webp" alt="all chargers" data-tilt>
-        </div>
-    </div>
-</section>
-<section class="container mb-5">
-    <div class="title_index_about title_index_about2">
-        <h3>
-            Our Plug And Play Charger Series
-        </h3>
-    </div>
-    <div class="row">
-        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-            <img class="x3_img" src="img/x3.webp" alt="x3" data-aos="fade-up" data-aos-duration="3000">
-        </div>
-        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-            <img class="x3_img" src="img/x4.webp" alt="x3" data-aos="fade-up" data-aos-duration="3000">
-        </div>
-    </div>
-</section>
-<section class="container mb-5">
-    <div class="title_index_about title_index_about2">
-        <h3>
-            Electric Vehicle Charger Manufacturer In India
-        </h3>
-    </div>
-    <div class="row">
-        <div class="photo_list_div">
-            <img src="img/n4.webp" alt="all chargers" data-tilt>
-        </div>
-    </div>
-</section>
-<section class="container mb-5">
-    <div class="title_index_about title_index_about2">
-        <h3>
-            X4 SERIES EV Charger EV Charger
-        </h3>
-    </div>
-    <div class="row g-4">
-        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-            <img class="x3_img" src="img/n5.webp" alt="x3" data-aos="fade-up" data-aos-duration="3000">
-        </div>
-        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-            <img class="x3_img" src="img/n6.webp" alt="x3" data-aos="fade-up" data-aos-duration="3000">
-        </div>
-    </div>
-</section>
-
-<!-- Management System end -->
+<!-- list end -->
 <!-- bg -->
 <section class="bg_div mb-5">
     <div class="bg_div_inner">
@@ -132,4 +113,4 @@
     </div>
 </section>
 <!-- bg end -->
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
